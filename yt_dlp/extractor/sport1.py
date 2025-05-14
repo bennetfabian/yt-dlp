2a28aceb65351ee54663443fb32ec50cc23614e6
Bennet Robin Fabian <bennet@bennet.dev>
2025-05-14 15:49:04 +0200

    Changed

--- yt_dlp/extractor/sport1.py
+++ yt_dlp/extractor/sport1.py
@@ -30,7 +30,7 @@ class Sport1IE(InfoExtractor):
         # video_description = extract_attributes(get_element_html_by_attribute('data-testid', 'video-description', webpage)["data-testid"] or '')
 
         # data = json.loads(get_element_by_id('__NEXT_DATA__', webpage))
-        print(get_element_html_by_id('__NEXT_DATA__', webpage))
+        # print(get_element_html_by_id('__NEXT_DATA__', webpage))
 
         return {
             'id': 'jahn-regensburg-karlsruher-sc-2-2-tore-highlights-2-bundesliga__97ED907D-7537-4301-BC0E-9BF5F13D4D73',
