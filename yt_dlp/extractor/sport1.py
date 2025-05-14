feb47509c45e4c36e28f68983ec1f5a0b77ff3c3
Bennet Robin Fabian <bennet@bennet.dev>
2025-05-14 15:51:18 +0200

    Changed

--- yt_dlp/extractor/sport1.py
+++ yt_dlp/extractor/sport1.py
@@ -17,9 +17,9 @@ class Sport1IE(InfoExtractor):
              'ext': 'mp4',
              'title': 'md5:5f0cabf75dae35179cf5675ba8f1f885',
              'description': 'Jahn Regensburg steigt aus der 2. Bundesliga ab. Ein Unentschieden gegen den Karlsruher SC besiegelt Platz 18.',
-             'display_id': 'jahn-regensburg-karlsruher-sc-2-2-tore-highlights-2-bundesliga__97ED907D-7537-4301-BC0E-9BF5F13D4D73'
-         },
-         'params': {'skip_download': 'm3u8'},
+             'display_id': 'jahn-regensburg-karlsruher-sc-2-2-tore-highlights-2-bundesliga__97ED907D-7537-4301-BC0E-9BF5F13D4D73',
+             'url': 'https://vod-dach.sport1.de/97ed907d-7537-4301-bc0e-9bf5f13d4d73/1-1080p.mp4'
+         }
     }]
 
     def _real_extract(self, url):
@@ -37,5 +37,6 @@ class Sport1IE(InfoExtractor):
             'title': 'Unentschieden besiegelt Regensburgs Platz 18',
             'description': 'Jahn Regensburg steigt aus der 2. Bundesliga ab. Ein Unentschieden gegen den Karlsruher SC besiegelt Platz 18.',
             'ext': 'mp4',
-            'display_id': 'jahn-regensburg-karlsruher-sc-2-2-tore-highlights-2-bundesliga__97ED907D-7537-4301-BC0E-9BF5F13D4D73'
+            'display_id': 'jahn-regensburg-karlsruher-sc-2-2-tore-highlights-2-bundesliga__97ED907D-7537-4301-BC0E-9BF5F13D4D73',
+            'url': 'https://vod-dach.sport1.de/97ed907d-7537-4301-bc0e-9bf5f13d4d73/1-1080p.mp4'
         }
