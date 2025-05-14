42dbc5513eb3dbb1a16d5d4b18119595bff51ce6
Bennet Robin Fabian <bennet@bennet.dev>
2025-05-14 15:42:59 +0200

    Changed

--- yt_dlp/extractor/sport1.py
+++ yt_dlp/extractor/sport1.py
@@ -24,16 +24,16 @@ class Sport1IE(InfoExtractor):
         display_id = self._match_id(url)
         webpage = self._download_webpage(url, display_id)
 
-        video_title = extract_attributes(get_element_html_by_attribute('data-testid', 'video-title', webpage)["data-testid"] or '')
-        video_description = extract_attributes(get_element_html_by_attribute('data-testid', 'video-description', webpage)["data-testid"] or '')
+        # video_title = extract_attributes(get_element_html_by_attribute('data-testid', 'video-title', webpage)["data-testid"] or '')
+        # video_description = extract_attributes(get_element_html_by_attribute('data-testid', 'video-description', webpage)["data-testid"] or '')
 
         # data = json.loads(get_element_by_id('__NEXT_DATA__', webpage))
         print(get_element_by_id('__NEXT_DATA__', webpage))
 
-        return {
-            'id': display_id,
-            'title': video_title,
-            'description': video_description,
-            'ext': 'mp4',
-            'display_id': display_id
-        }
+        # return {
+        #     'id': display_id,
+        #     'title': video_title,
+        #     'description': video_description,
+        #     'ext': 'mp4',
+        #     'display_id': display_id
+        # }
